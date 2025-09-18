962382743f3421e7c4ff7b237290c29013dac359
Kevin D <76125913+kevin8999@users.noreply.github.com>
2025-09-17 21:21:10 -0400

    chore: Add link to `roulette.py` from `main.py`
    Adds `play_roulette()` function in `roulette.py` to the global namespace.
    
    NOTE FOR FUTURE MAINTAINERS: Please use something else other than the global namespace when accessing functions.
    USE OF THE GLOBAL NAMESPACE SHOULD BE AVOIDED FOR SECURITY REASONS.

--- casino/main.py
+++ casino/main.py
@@ -1,4 +1,8 @@
+# Import casino games
 from casino.games.blackjack import play_blackjack
+from casino.games.roulette  import play_roulette
+
+# Import utilities
 from casino.utils import cprint, cinput, clear_screen
 
 CASINO_HEADER = """
@@ -14,7 +18,7 @@ ENTER_OR_QUIT_PROMPT = "[E]nter   [Q]uit: \n"
 INVALID_CHOICE_PROMPT = "\nInvalid input. Please try again. \n\n"
 GAME_CHOICE_PROMPT = "Please choose a game to play:\n"
 
-games = ["blackjack"]
+games = ["blackjack", "roulette"]
 
 
 def welcome():
