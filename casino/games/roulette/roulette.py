e48cbc2b82d0f690cc06c33ba4fd977f4ef9b962
Connor Kress <ckress@ufl.edu>
2025-10-09 18:48:57 -0400

    fix roulette type errors

--- casino/games/roulette/roulette.py
+++ casino/games/roulette/roulette.py
@@ -1,26 +1,7 @@
-"""
-roulette.py
-
-This file provides a way to play roulette.
-
-WARNING: do not run this file as main. Instead, change to root directory
-of `TERMINALCASINO/`, and run the following command:
-
-```
-python -m casino.main
-```
-
-Then, pick "Roulette" as the game you want to run.
-"""
-
-
 import random
-import os
-import shutil
-from typing import List
+from typing import List, Optional
 from time import sleep
 
-from casino.card_assets import assign_card_art
 from casino.types import GameContext
 from casino.utils import clear_screen, cprint, cinput
 from casino.accounts import Account
@@ -30,6 +11,47 @@ ROULETTE_HEADER = """
 │     ♠ R O U L E T T E ♠     │
 └─────────────────────────────┘
 """
+
+STANDARD_AMERICAN_ROULETTE_WHEEL = [
+    ("0", "green"),
+    ("28", "black"),
+    ("9", "red"),
+    ("26", "black"),
+    ("30", "red"),
+    ("11", "black"),
+    ("7", "red"),
+    ("20", "black"),
+    ("32", "red"),
+    ("17", "black"),
+    ("5", "red"),
+    ("22", "black"),
+    ("34", "red"),
+    ("15", "black"),
+    ("3", "red"),
+    ("24", "black"),
+    ("36", "red"),
+    ("13", "black"),
+    ("1", "red"),
+    ("00", "green"),
+    ("27", "red"),
+    ("10", "black"),
+    ("25", "red"),
+    ("29", "black"),
+    ("12", "red"),
+    ("8", "black"),
+    ("19", "red"),
+    ("31", "black"),
+    ("18", "red"),
+    ("6", "black"),
+    ("21", "red"),
+    ("33", "black"),
+    ("16", "red"),
+    ("4", "black"),
+    ("23", "red"),
+    ("35", "black"),
+    ("14", "red"),
+    ("2", "black")
+]
 
 
 class Roulette:
@@ -62,9 +84,8 @@ class Roulette:
         """
         Initializes roulette
         """
-
         # Will be populated with numbers and colors
-        self.wheel = []
+        self.wheel = list[tuple[str, str]]()
 
         # Colors on wheel. Includes initials
         self.valid_colors = ["red", "green", "black", "r", "g", "b"]
@@ -74,7 +95,7 @@ class Roulette:
 
         # Current round's bets
         self.bets = {}
-        self.winning_value = None
+        self.winning_value: Optional[tuple[str, str]] = None
 
     @staticmethod
     def normalize_color(input_value: str) -> str:
@@ -168,7 +189,8 @@ class Roulette:
                 player_balances += account.balance
             
             if player_balances == 0:
-                print("ERROR: All players have gone bankrupt. You cannot play any more roulette.")
+                print("ERROR: All players have gone bankrupt. "
+                      "You cannot play any more roulette.")
                 sleep(3)
                 return "BANKRUPT"
 
@@ -178,7 +200,7 @@ class Roulette:
 
             will_bet = input(f"Would you like to bet, Player {i+1} (y/N): ")
 
-            if will_bet == "" or will_bet.upper() == "N" or will_bet.upper() == "NO":
+            if will_bet == "" or will_bet.lower() in {"n", "no"}:
                 print("User skipped betting. Moving to next user...", end="\n\n")
                 i += 1
                 continue
@@ -198,7 +220,8 @@ class Roulette:
                 if (bet_amount < 0):
                     raise ValueError
             except ValueError:
-                print(f"ERROR: '{bet_amount}' is not a valid number. Please enter a positive integer.", end="\n\n")
+                print(f"ERROR: '{bet_amount}' is not a valid number. "
+                      "Please enter a positive integer.", end="\n\n")
                 continue
 
             # Check that account has enough money to bet
@@ -206,23 +229,28 @@ class Roulette:
                 self.accounts[i].withdraw(bet_amount)
             except ValueError as error:
                 if error.args and error.args[0] == "Insufficient balance":
-                    print(f"Insufficient balance to place bet. Please enter a bet less thanor equal to {self.accounts[i].balance}")
+                    print("Insufficient balance to place bet. Please enter a "
+                          f"bet less thanor equal to {self.accounts[i].balance}")
                 continue
 
             print(f"Successfully withdrew {bet_amount} coins from Player {i+1}.")
-            print(f"\tPlayer {i+1} remaining balance: {self.accounts[i].balance} coins.")
+            print(f"\tPlayer {i+1} remaining balance: "
+                  f"{self.accounts[i].balance} coins.")
 
             # Ask for desired bet type
             bet_type = ""
             valid_bet_types = ["C", "COLOR", "N", "NUMBER"]
 
             while True:
-                bet_type = input("Would you like to bet on a color or a number? (C or N): ")
+                bet_type = input(
+                    "Would you like to bet on a color or a number? (C or N): "
+                ).strip()
 
                 if (bet_type.upper() in valid_bet_types):
                     break
                 else:
-                    print("Error: Invalid bet type. Choose either 'color' or 'number'.")
+                    print("Error: Invalid bet type. "
+                          "Choose either 'color' or 'number'.")
 
             ############################################################
 
@@ -230,25 +258,28 @@ class Roulette:
             bet_value = ""
 
             # Color betting
-            if bet_type.lower() == "c" or bet_type.lower() == "color":
+            if bet_type.lower() in {"c", "color"}:
                 while True:
-                    bet_value = input("Enter color you want to bet on (red, black, green): ")
-
+                    bet_value = input(
+                        "Enter color you want to bet on (red, black, green): "
+                    )
                     if bet_value.lower() in self.valid_colors:
                         break
                     else:
                         print("Error: Chosen color is not red, green, or black.")
             
             # Number betting
-            elif bet_type.upper() == "N" or bet_type.upper() == "NUMBER":
+            elif bet_type.lower() in {"n", "number"}:
                 while True:
                     bet_value = input("Enter number you would like to bet on: ")
 
                     if bet_value in self.valid_numbers:
                         break
                     else:
-                        print("Error: You may only enter one of the following numbers.")
-                        sorted_numbers = sorted(self.valid_numbers, key=roulette_sort_key)
+                        print("Error: You may only enter one of the following "
+                              "numbers.")
+                        sorted_numbers = sorted(self.valid_numbers,
+                                                key=self.roulette_sort_key)
                         valid_numbers_str = ", ".join(sorted_numbers)
                         print("\t" + valid_numbers_str)
          
@@ -259,20 +290,19 @@ class Roulette:
                 "value"  : Roulette.normalize_color(bet_value.lower()),
                 "amount" : bet_amount
             }
-
             i += 1  # Move to next user
 
     def payout(self) -> None:
         """
         Pay out all players who picked the right color or number.
         """
+        assert self.winning_value is not None
         winning_number = self.winning_value[0]
         winning_color  = self.winning_value[1]
 
         print("Paying out all winners...")
-
         i = 0
-        for account_id, bet in self.bets.items():
+        for _, bet in self.bets.items():
             bet_type   = bet["type"]
             bet_value  = bet["value"]
             bet_amount = bet["amount"]
@@ -290,8 +320,9 @@ class Roulette:
                 win_multiplier += 35
 
             if win_multiplier > 1:
-                self.accounts[i].deposit(bet_amount * win_multiplier)
-                print(f"\tPlayer {i+1}: Won {bet_amount * win_multiplier} coins.")
+                win_amount = bet_amount * win_multiplier
+                self.accounts[i].deposit(win_amount)
+                print(f"\tPlayer {i+1}: Won {win_amount} coins.")
             else:
                 print(f"\tPlayer {i+1}: Lost {bet_amount} coins.")
             
@@ -301,70 +332,28 @@ class Roulette:
 
 
 class AmericanRoulette(Roulette):
-    """
-    Plays roulette using American rules
-    """
+    """Plays roulette using American rules."""
 
     def __init__(self, accounts: List[Account]):
         super().__init__(accounts)
-
-        # Use the standard American roulette wheel
-        self.wheel = [
-            ("0", "green"),
-            ("28", "black"),
-            ("9", "red"),
-            ("26", "black"),
-            ("30", "red"),
-            ("11", "black"),
-            ("7", "red"),
-            ("20", "black"),
-            ("32", "red"),
-            ("17", "black"),
-            ("5", "red"),
-            ("22", "black"),
-            ("34", "red"),
-            ("15", "black"),
-            ("3", "red"),
-            ("24", "black"),
-            ("36", "red"),
-            ("13", "black"),
-            ("1", "red"),
-            ("00", "green"),
-            ("27", "red"),
-            ("10", "black"),
-            ("25", "red"),
-            ("29", "black"),
-            ("12", "red"),
-            ("8", "black"),
-            ("19", "red"),
-            ("31", "black"),
-            ("18", "red"),
-            ("6", "black"),
-            ("21", "red"),
-            ("33", "black"),
-            ("16", "red"),
-            ("4", "black"),
-            ("23", "red"),
-            ("35", "black"),
-            ("14", "red"),
-            ("2", "black")
-        ]
-
-        self.valid_numbers = [number for (number, color) in self.wheel]
+        self.wheel = STANDARD_AMERICAN_ROULETTE_WHEEL
+        self.valid_numbers = [number for (number, _) in self.wheel]
 
 
-def play_roulette(contexts : List[GameContext]) -> None:
+def play_roulette(context: GameContext) -> None:
     continue_game = True
 
     # Temporary fix
-    # TODO: fix argument in play_roulette to only except `List[GameContext]` and not `GameContext`
-    contexts = [contexts]
+    # TODO: fix argument in play_roulette to only except `List[GameContext]`
+    # and not `GameContext`
+    contexts = [context]
 
     # Access account data
     accounts = [account.account for account in contexts]
 
-    if type(accounts) != list:
-        raise ValueError(f"accounts is not a list. accounts is a {type(accounts)}")
+    if not isinstance(accounts, list):
+        raise ValueError("accounts is not a list. "
+                         f"accounts is a {type(accounts)}")
 
     roulette = AmericanRoulette(accounts)
     while continue_game:
@@ -374,7 +363,7 @@ def play_roulette(contexts : List[GameContext]) -> None:
         # Input to stop loop from running constantly
         choice = cinput("Press [Enter] to start a new round and [q] to quit: ")
 
-        if choice.upper() == "q" or choice.upper() == "QUIT":
+        if choice.lower() in {"q", "quit"}:
             continue_game = False
             break
 
@@ -394,11 +383,11 @@ def play_roulette(contexts : List[GameContext]) -> None:
             if play_again.upper() not in valid_choices:
                 print("Please enter 'Yes' or 'No'.")
                 continue
-            if play_again.upper() == "N" or play_again.upper() == "NO":
+            if play_again.lower() in {"n", "no"}:
                 print("Quitting roulette...")
                 continue_game = False
                 break
-            elif play_again.upper() == "" or play_again.upper() == "Y" or play_again.upper() == "YES":
+            elif play_again == "" or play_again.lower() in {"y", "yes"}:
                 continue_game = True
                 break
 
