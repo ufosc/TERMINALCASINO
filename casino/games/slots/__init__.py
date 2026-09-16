b294f31c05007a4a811b2199df2cda8864c7446e
Kevin Duong <preprocessors_lulavim@aleeas.com>
2026-09-15 21:51:50 -0400

    Revert "Refactor slots to match blackjack's base/variants/views structure (#191)"

--- casino/games/slots/__init__.py
+++ casino/games/slots/__init__.py
@@ -1,3 +1,4 @@
-from .variants import play_slots, play_slots_expanded
+from .slots import play_slots
+from .slots_expanded import play_slots_expanded
 
 __all__ = ["play_slots", "play_slots_expanded"]
--- casino/games/slots/variants/__init__.py
+++
@@ -1,9 +0,0 @@
-from .standard import StandardSlots, play_slots
-from .expanded import ExpandedSlots, play_slots_expanded
-
-__all__ = [
-    "StandardSlots",
-    "play_slots",
-    "ExpandedSlots",
-    "play_slots_expanded",
-]
